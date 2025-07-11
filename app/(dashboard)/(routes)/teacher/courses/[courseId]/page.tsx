0b14107a5c32daf495489657c518ec2ba20a4bd8
Jesse Cristobal <cristobaljesse@gmail.com>
2025-07-11 04:40:46 -0400

    Implemented course description form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { LayoutDashboard } from "lucide-react";
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string}}) => {
 
@@ -58,6 +59,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string}}) => {
                         </h2>
                     </div>
                     <TitleForm 
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <DescriptionForm 
                         initialData={course}
                         courseId={course.id}
                     />
