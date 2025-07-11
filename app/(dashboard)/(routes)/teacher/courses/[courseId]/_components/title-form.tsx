038a3328d0d8ab5730960f93b03eb359052490b3
Jesse Cristobal <cristobaljesse@gmail.com>
2025-07-11 04:34:40 -0400

    Debugged typo causing 404 error

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -48,7 +48,7 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            await axios.patch(`/api/course/${courseId}, values`);
+            await axios.patch(`/api/courses/${courseId}`, values);
             toast.success("Course updated");
             toggleEdit();
             router.refresh();
