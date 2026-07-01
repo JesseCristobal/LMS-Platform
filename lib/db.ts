d3f9f22d94907ec978ff0d7943a959a08c5fbfb5
Jesse Cristobal <cristobaljesse@gmail.com>
2026-07-01 14:40:49 -0400

    Refactored database code

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = new PrismaClient({ adapter });
+
 
 declare global {
     // eslint-disable-next-line no-var
     var prisma: PrismaClient | undefined;
 };
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db = globalThis.prisma || prisma;
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
